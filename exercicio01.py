986f001937485e4edc7bb47338afd3330e07a3f5
Bruno Campagnoli Soares da Costa Esposito <122655406+brunoesposito3012@users.noreply.github.com>
2023-04-27 13:47:36 -0300

    Add files via upload

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+Distancia = float(input('digite a distância percorrida:'))
+tempo = float(input('digite o tempo gasto:'))
+Vm = Distancia/tempo
+print(f'A sua velocidade média foi de:{Vm} k/h')
---
+++ exercicio06.py
@@ -0,0 +1,6 @@
+Nome = input(('informe seu nome:'))
+N1 = float(input('Informe sua nota de Laboratório de Programação:'))
+N2 = float(input('Informe sua nota de Tgs:'))
+N3 = float(input('Informe sua nota de web development:'))
+Média = (N1+N2+N3)/3
+print(f'Sua média foi:{Média:.2f}')
